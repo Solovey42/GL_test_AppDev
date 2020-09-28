2d7b94979802eadb78a1332f36259cdd5407b587
Slava <solovey.1999@mail.ru>
2020-09-28 19:11:25 +0700

    Add solution

--- src/task2.kt
+++ src/task2.kt
@@ -1,3 +1,21 @@
-fun main(args: Array<String>) {
-
+fun main() {
+    val km = 109
+    println("Введите cкорость")
+    val v: Int? = readLine()?.toInt()
+    println("Введите время")
+    val t = readLine()?.toInt()
+    val s: Int? = t!! * v!!
+    if (s != null) {
+        if (s < 0) {
+            print(km + s % km)
+        }
+    }
+    if (s != null) {
+        if (s > 0) {
+            println(s-km*(s/km))
+        }
+    }
 }
+
+
+
