aa748f2dee8e498bf2ceef8320adb70eaf650184
Slava <solovey.1999@mail.ru>
2020-09-28 18:02:07 +0700

    Create main

--- src/task1.kt
+++ src/task1.kt
@@ -1,3 +1,20 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("Введите длинну первой стороны бассейна")
+    val n: String? = readLine()
+    println("Введите длинну второй стороны бассейна")
+    val m = readLine()
+    println("Введите расстояние до длинной стороны бассейна")
+    val x = readLine()
+    println("Введите расстояние до короткой22 стороны бассейна")
+    val y = readLine()
+    if (n != null) {
+        if (m != null) {
+            if (x != null) {
+                if (y != null) {
+                    println("Минимально растояние до бортика: " + compare(n.toInt(), m.toInt(), x.toInt(), y.toInt()))
+                }
+            }
+        }
+    }
+    fun compare(n: Int, m: Int, x: Int, y: Int) {}
 }
